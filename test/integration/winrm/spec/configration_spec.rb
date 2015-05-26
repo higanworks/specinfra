60ed33bbe1d416ec8094ad425c5d39840b4754f8
sawanoboly <sawanoboriyu@higanworks.com>
2015-05-26 16:15:25 +0900

    get os_info from backend

--- test/integration/winrm/spec/configration_spec.rb
+++ test/integration/winrm/spec/configration_spec.rb
@@ -1,3 +1,5 @@
-#describe Specinfra::Configration do
-  # expect(Specinfra.configration.os).to eq {:family => 'windows'}
-#end
+describe "Configration" do
+  it "os family get correctly from backend" do
+    expect(Specinfra.backend.os_info[:family]).to eq 'windows'
+  end
+end
