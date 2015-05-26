6b8961e0f297ea9179fe34f806779c276603e1c8
sawanoboly <sawanoboriyu@higanworks.com>
2015-05-26 15:43:41 +0900

    update specs

--- test/integration/winrm/spec/command_spec.rb
+++ test/integration/winrm/spec/command_spec.rb
@@ -1,2 +1,14 @@
-describe Specinfra::Backend::Winrm do
+describe specinfra::backend::winrm do
+  describe "#create_command_class" do
+    expect(Specinfra.backend.command.send(:create_command_class, 'user')).to eq "Specinfra::Command::Windows::Base::User"
+    expect(Specinfra.backend.command.send(:create_command_class, 'user')).to eq "Specinfra::Command::Windows::Base::User"
+  end
+
+  describe "#get_command for powershell" do
+    subject(Specinfra.backend.command.get(:check_user_exists, 'testuser'))
+    expect(subject.class).to eq "Specinfra::Backend::PowerShell::Command"
+  end
+
+  describe "#run_command" do
+  end
 end
